7fe8fd4f534d0a4c363634085fe558467af5ae9c
Jeremy Jusak <jusakj@comcast.net>
2022-01-25 21:19:40 -0800

    Initial Implementation of QuadParser

---
+++ QuadParser/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+
+namespace QuadParser
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string inputFileName = "participants.txt";
+            const string outputFileBaseName = "Participants.txt";
+
+            string currentDirectory = Directory.GetCurrentDirectory();
+            string inputFilePath = Path.Combine(currentDirectory, inputFileName);
+
+            if (!File.Exists(inputFilePath))
+            {
+                Console.WriteLine("Couldn't find participants.txt in current directory.");
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+
+            string[] fileLines = File.ReadAllLines(inputFilePath);
+            List<Player> players = SplitIntoPlayers(fileLines);
+            List<Quad> quads = SplitIntoQuads(players);
+
+            int quadNumber = 1;
+            foreach (Quad quad in quads)
+            {
+                string quadFileName = $"Q{quadNumber}{outputFileBaseName}";
+                string quadFilePath = Path.Combine(currentDirectory, quadFileName);
+
+                string[] quadLines = quad.Players.SelectMany(x => x.SourceBlockLines.Concat(new List<string> { "" })).ToArray();
+                File.WriteAllLines(quadFilePath, quadLines);
+
+                quadNumber++;
+            }
+        }
+
+        private static List<Quad> SplitIntoQuads(IReadOnlyList<Player> players)
+        {
+            List<Quad> quads = new List<Quad>();
+
+            if (players == null || players.Count == 0)
+            {
+                return quads;
+            }
+
+            List<Player> playersOrderedByRating = players.OrderByDescending(x => x.Rating).ToList();
+            if (playersOrderedByRating.Count <= 6)
+            {
+                quads.Add(new Quad(playersOrderedByRating));
+                return quads;
+            }
+
+            int index = 0;
+            while (index < playersOrderedByRating.Count - 8)
+            {
+                quads.Add(new Quad(playersOrderedByRating.Skip(index).Take(4).ToList()));
+                index += 4;
+            }
+
+            int remainder = playersOrderedByRating.Count % 4;
+            switch (remainder)
+            {
+                case 0:
+                    quads.Add(new Quad(playersOrderedByRating.Skip(index).Take(4).ToList()));
+                    quads.Add(new Quad(playersOrderedByRating.Skip(index + 4).Take(4).ToList()));
+                    break;
+                case 1:
+                    // last quad should be a five person quad
+                    quads.Add(new Quad(playersOrderedByRating.Skip(index).Take(5).ToList()));
+                    break;
+                case 2:
+                    // last quad should be a 6 person quad
+                    quads.Add(new Quad(playersOrderedByRating.Skip(index).Take(6).ToList()));
+                    break;
+                case 3:
+                    // last quad should be a 3 person quad
+                    quads.Add(new Quad(playersOrderedByRating.Skip(index).Take(4).ToList()));
+                    quads.Add(new Quad(playersOrderedByRating.Skip(index + 4).Take(3).ToList()));
+                    break;
+            }
+
+            return quads;
+        }
+
+        private static List<Player> SplitIntoPlayers(IReadOnlyList<string> fileLines)
+        {
+            List<Player> players = new List<Player>();
+            List<string> blockLines = new List<string>();
+            for (int i = 0; i < fileLines.Count; i++)
+            {
+                if (fileLines[i].ToLowerInvariant().StartsWith("name"))
+                {
+                    if (blockLines.Count > 0)
+                    {
+                        players.Add(new Player(blockLines.ToArray()));
+                        blockLines.Clear();
+                    }
+                }
+
+                if (fileLines[i].Trim() != string.Empty)
+                {
+                    blockLines.Add(fileLines[i]);
+                }
+            }
+
+            if (blockLines.Count > 0)
+            {
+                players.Add(new Player(blockLines.ToArray()));
+                blockLines.Clear();
+            }
+
+            return players;
+        }
+
+        private class Quad
+        {
+            public Quad(IReadOnlyCollection<Player> players)
+            {
+                Players = players;
+            }
+
+            public IReadOnlyCollection<Player> Players { get; }
+        }
+
+        private class Player
+        {
+            public Player(string[] blockLines)
+            {
+                SourceBlockLines = blockLines.Where(x => x.Trim() != "").ToArray();
+
+                NameLine = blockLines.SingleOrDefault(x => x.ToLowerInvariant().StartsWith("name"));
+                IdNumberLine = blockLines.SingleOrDefault(x => x.ToLowerInvariant().StartsWith("id"));
+                TeamLine = blockLines.SingleOrDefault(x => x.ToLowerInvariant().StartsWith("team"));
+                AgeLine = blockLines.SingleOrDefault(x => x.ToLowerInvariant().StartsWith("age"));
+
+                Rating = ParseRating(blockLines.SingleOrDefault(x => x.ToLowerInvariant().StartsWith("rating")));
+            }
+
+            public string[] SourceBlockLines { get; }
+
+            public string NameLine { get; }
+
+            public int Rating { get; }
+
+            public string IdNumberLine { get; }
+
+            public string TeamLine { get; }
+
+            public string AgeLine { get; }
+
+            private int ParseRating(string inputLine)
+            {
+                if (string.IsNullOrEmpty(inputLine))
+                {
+                    return 0;
+                }
+
+                string ratingValue = new string(inputLine.SkipWhile(x => x != '=').Skip(1).ToArray()).Trim();
+                if (ratingValue.StartsWith("unr"))
+                {
+                    return 0;
+                }
+
+                if (!int.TryParse(ratingValue, out int rating))
+                {
+                    return 0;
+                }
+
+                return rating;
+            }
+        }
+    }
+}
