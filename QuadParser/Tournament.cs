c283bd1de8ff8cb6a567b003343d5182c6558018
Jeremy Jusak <jusakj@comcast.net>
2022-01-27 10:57:34 -0800

    Move classes out to own files

---
+++ QuadParser/Tournament.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace QuadParser
+{
+    public class Quad
+    {
+        public Quad(IReadOnlyCollection<Player> players)
+        {
+            Players = players;
+        }
+
+        public IReadOnlyCollection<Player> Players { get; }
+    }
+}
